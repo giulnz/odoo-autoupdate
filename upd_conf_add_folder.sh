9868b190be8825388e4e063d711edf4d99bef3eb
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 16:19:54 +0100

    Update upd_conf_add_folder.sh

--- upd_conf_add_folder.sh
+++ upd_conf_add_folder.sh
@@ -4,7 +4,7 @@
 IFS=','
 
 # Folder list
-# combine OCA_FOLDER and CUSTOM_FOLDERS into ALL_FOLDERS
+# combine OCA_FOLDER and CUSTOM_FOLDERS into var_folders
 var_folders=("$OCA_FOLDER" "${CUSTOM_FOLDERS[@]}")
 variable_folders=$(ls -d $var_folders/*/ 2>/dev/null)
 fix_folders="${FIX_FOLDERS[*]}"
