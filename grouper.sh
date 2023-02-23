89436deda8a78765d74944bfe65e9e42516fbce2
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 14:00:52 +0100

    Update grouper.sh

--- grouper.sh
+++ grouper.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Importa il file di configurazione
+current_dir=$(pwd)
+
+# Create a log file and open it for writing
+log_file="$LOGFILE$(date +"%Y-%m-%d_%H-%M-%S").log"
+exec > >(tee -i $log_file)
+
+# Import the configuration file
 . autoupdate.conf
 
-# Fermo il servizio odoo
+# Stop the odoo service (as super user)
 sudo /bin/systemctl stop $ODOO_SERVICE.service
 
-# Aggiorno OCA (UTENTE ODOO)
-su - $ODOO_USER -s /bin/bash $ODOO_PATH/odoo-autoupdate/updateOCA.sh
+# Update OCA (AS ODOO USER)
+su - $ODOO_USER -s /bin/bash $current_dir/updateOCA.sh
 
-# Aggiorno lista addons su conf odoo
-sudo $ODOO_PATH/odoo-autoupdate/upd_conf_add_folder.sh
+# Aggiorno lista addons su conf odoo (as super user )
+sudo $current_dir/upd_conf_add_folder.sh
 
-# Aggiorno Odoo (UTENTE ODOO)
-su - $ODOO_USER -s /bin/bash $ODOO_PATH/odoo-autoupdate/update_odoo.sh
+# Update ODOO (AS ODOO USER)
+su - $ODOO_USER -s /bin/bash $current_dir/update_odoo.sh
 
-# Aggiorno DB (UTENTE ODOO)
-su - $ODOO_USER -s /bin/bash $ODOO_PATH/odoo-autoupdate/updateDB.sh
+# Update DB (AS ODOO USER)
+su - $ODOO_USER -s /bin/bash $current_dir/updateDB.sh
 
-# Riavvio il servizio odoo
+# Riavvio il servizio odoo (as super user )
 sudo /bin/systemctl start $ODOO_SERVICE.service
