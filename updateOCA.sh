5a1a1741953de4ce989128858bc1b42456eaaffa
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 11:45:00 +0100

    Update updateOCA.sh

--- updateOCA.sh
+++ updateOCA.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-cd /opt/odoo/addons
+cd $OCA_FOLDER
+cd ..
 gh repo list OCA --limit 500 | while read -r repo _; do
-  if [ "$repo" == "OCA/OCB" ] || [[ "$repo" =~ ^OCA/l10n.* ]] && [ "$repo" != "OCA/l10n-italy" ]; then
+  if $EXCLUDE_REPO; then
     echo "Skip $repo"
     continue
   fi
   echo "$repo"
-  gh repo clone "$repo" "$repo" -- --branch 14.0 --depth=1 || (
+  gh repo clone "$repo" "$repo" -- --branch $ODOO_VERS --depth=1 || (
     cd "$repo"
     git checkout || true
     git pull
   )
-done | tee /opt/odoo/log/log_updateOCA$(date +"%Y-%m-%d_%H-%M-%S").txt
+done
