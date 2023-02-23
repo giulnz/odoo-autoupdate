c6362534be65a381615900bf01cd197c3a3a4110
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 17:05:08 +0100

    Update update_custom_addons.sh

--- update_custom_addons.sh
+++ update_custom_addons.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dir=$(dirname $0)
+cd $dir
+
+# Import the configuration file
+. autoupdate.conf
+
 # loop through all folders in $CUSTOM_FOLDER
 for folder in "${CUSTOM_FOLDER[@]}"; do
     echo "Updating folder: $folder"
