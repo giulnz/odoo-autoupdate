1dabe3f9a186bbbf3bc7891bb940145f830cbee8
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 12:42:32 +0100

    Update updateOCA.sh

--- updateOCA.sh
+++ updateOCA.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verify that the input variables are set
+if [[ -z "$OCA_FOLDER" || -z "$ODOO_VERS" || -z "$ODOO_CONF" ]]; then
+  echo "Error: one or more input variables not set" >&2
+  exit 1
+fi
+
 cd $OCA_FOLDER
 cd ..
 gh repo list OCA --limit 500 | while read -r repo _; do
