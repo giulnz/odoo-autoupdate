35d5ce9b70ce513a98cbe2473152690a6e3c7fff
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-23 14:20:54 +0100

    Update odoo-autoupdate.sh

--- odoo-autoupdate.sh
+++ odoo-autoupdate.sh
@@ -5,7 +5,7 @@
 #-------------------------------------------------------------------------------
 # git clone https://github.com/ser-tec/odoo-autoupdate.git
 # makes the file executable:
-# sudo chmod +x odoo-install.sh
+# sudo chmod +x odoo-autoupdate.sh
 # Execute the script to update Odoo:
 # ./odoo-autoupdate.sh
 ################################################################################
