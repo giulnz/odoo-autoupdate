3759f1f7c6b2f234f8ec452def2420e4749b2170
ser-tec <113360845+ser-tec@users.noreply.github.com>
2023-02-24 13:41:09 +0100

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -32,27 +32,45 @@ var_list=("LOGFILE" "ODOO_PATH" "ODOO_SERVICE" "ODOO_USER" "ODOO_CONF" "OCA_FOLD
     done
 
 # Stop the odoo service (as super user)
-sudo /bin/systemctl stop $ODOO_SERVICE.service
+if sudo /bin/systemctl stop $ODOO_SERVICE.service; then
+  echo "Odoo service stopped successfully"
+else
+  echo "Failed to stop Odoo service"
+fi
 
 # Update OCA (AS ODOO USER)
+echo 
 echo Update OCA
+echo 
 su - $ODOO_USER -s /bin/bash $dir/updateOCA.sh
 
 # Update CUSTOM_FOLDER (AS ODOO USER)
+echo 
 echo Update CUSTOM_FOLDER
+echo 
 su - $ODOO_USER -s /bin/bash $dir/update_custom_addons.sh
 
 # Update ODOO (AS ODOO USER)
+echo 
 echo Update ODOO
+echo 
 su - $ODOO_USER -s /bin/bash $dir/update_odoo.sh
 
 # Update addons list on conf odoo (as super user )
+echo 
 echo Update addons list
+echo 
 sudo $dir/upd_conf_add_folder.sh
 
 # Update DB (AS ODOO USER)
+echo 
 echo Update DB
+echo 
 su - $ODOO_USER -s /bin/bash $dir/updateDB.sh
 
 # Riavvio il servizio odoo (as super user )
-sudo /bin/systemctl start $ODOO_SERVICE.service
+if sudo /bin/systemctl start $ODOO_SERVICE.service; then
+  echo "Odoo service started successfully"
+else
+  echo "Failed to start Odoo service"
+fi
